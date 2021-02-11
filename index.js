1a8d9945dbd5b30123209d98e870eaf0cdae4ecb
sarah-maau <maurysarah@hotmail.fr>
2021-02-11 16:09:08 +0100

    use cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
+const cors = require('cors');
+
+app.use(cors());
 
 const expressSwagger = require('express-swagger-generator')(app);
 
